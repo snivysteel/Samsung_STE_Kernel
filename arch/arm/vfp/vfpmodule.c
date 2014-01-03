ddd5dd680089830103b2f69f4287f3c848dc9014
KINGbabasula <kingbabasula@gmail.com>
2014-01-03 14:54:59 +0100

    Update to 3.0.57

--- arch/arm/vfp/vfpmodule.c
+++ arch/arm/vfp/vfpmodule.c
@@ -590,6 +590,8 @@ static int __init vfp_init(void)
 			 */
 			if (((fmrx(MVFR0) & MVFR0_A_SIMD_MASK)) == 1)
 				elf_hwcap |= HWCAP_VFPv3D16;
+			else
+				elf_hwcap |= HWCAP_VFPD32;
 		}
 #endif
 		/*
